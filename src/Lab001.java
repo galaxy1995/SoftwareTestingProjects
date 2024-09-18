235400a1b6c7ce037729e9af14dd9ab60db28a52
Geetanjlee <geet26patil@gmail.com>
2024-09-18 18:33:11 +0530

    Lab001 Code

---
+++ src/Lab001.java
@@ -0,0 +1,13 @@
+public class Lab001 {
+ public static void main(String [] args) {
+
+
+    /* System.out.println("Hello JAVA");
+     *System.out.println("Hello JAVA");
+     *System.out.println("Hello JAVA");
+     * System.out.println("Hello JAVA");
+      System.out.println(); */
+
+
+ }
+}
---
+++ src/Lab002.java
@@ -0,0 +1,16 @@
+public class Lab002
+{
+    public static void main(String[] args)
+    { int A = 20;
+      int G = 20;
+
+
+            System.out.println("Hello JAVA");
+            System.out.println(A);
+            System.out.println(G);
+            System.out.println("Result is "+(A+G));
+
+
+
+    }
+}
